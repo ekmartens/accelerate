3fc969aab942aaeb17078b738fcfe05756259b14
ekmartens <ekmrtns@gmail.com>
2017-10-24 19:28:31 -0700

    finished case-studies arcive

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -5,34 +5,33 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
+	<div id="case-studies-archive" class="site-content">
 		<div class="main-content" role="main">
 
       <?php while ( have_posts() ) : the_post();
         $services = get_field('services');
-        $client = get_field('client');
-        $link = get_field('site_link');
         $image1 = get_field('image_1');
         $size = "full";
         ?>
       <article class="case-study">
         <aside class="case-study-sidebar">
-          <h2><?php the_title(); ?></h2>
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <h3><?php echo $services; ?></h3>
-          <h4>Client: <?php echo $client; ?></h4>
           <p><?php the_excerpt(); ?></p>
-          <p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site > </a></p>
+          <p class="read-more-link"><a href="<?php the_permalink(); ?>">View Project > </a></p>
         </aside>
       </article>
+
+      <div class="case-study-imgs">
+        <a href="<?php the_permalink(); ?>"><?php if($image1) {
+          echo wp_get_attachment_image( $image1, $size );
+        } ?></a>
+
+      </div>
       <?php endwhile; // end of the loop. ?>
 
 
-      <div class="case-study-imgs">
-        <?php if($image1) {
-          echo wp_get_attachment_image( $image1, $size );
-        } ?>
 
-      </div>
 
 
 
